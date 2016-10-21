2079a1893fda70e77331574b74499a4d1f940f6a
Bruno Oliveira <brunooliveira.mac@gmail.com>
2016-10-20 23:02:09 -0200

    creating gulp builds

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,27 @@
 var gulp = require('gulp'),
-		imagemin = require('gulp-imagemin'); 
+		imagemin = require('gulp-imagemin'),
+		clean = require('gulp-clean'); 
+
+				//dependency -- it will be executed before
+gulp.task('copy',['clean'], function(){
+			//all directories and files
+	return gulp.src('public/**/*')
+		  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function(){
+
+	var stream = gulp.src('dist') 				  //removing assycronous (return)
+	                 .pipe(clean());			  //removing assycronous
+	return stream;				 				  //removing assycronous
+});
 
 // npm run gulp build-img
-gulp.task('build-img', function(){
+gulp.task('build-img',['copy'], function(){
 	 
-	gulp.src('public/img/**/*') //<-- Process files inside folder **
+	gulp.src('dist/img/**/*') //<-- Process files inside folder **
 		.pipe(imagemin()) //<--module 
-		.pipe( gulp.dest('public/img') );  
+		.pipe( gulp.dest('dist/img') );  
 
 }); 
 
