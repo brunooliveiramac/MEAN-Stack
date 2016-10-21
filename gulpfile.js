c77c528b68bb7595aad706b25cb88a20a0554c5e
Bruno Oliveira <brunooliveira.mac@gmail.com>
2016-10-20 23:54:06 -0200

    build: img-html-js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp'),
 		imagemin = require('gulp-imagemin'),
-		clean = require('gulp-clean'); 
+		clean = require('gulp-clean'),
+		concat = require('gulp-concat'),
+		htmlReplace = require('gulp-html-replace'); 
 
+
+gulp.task('default', ['copy'], function(){
+
+		gulp.start('build-img', 'build-js', 'build-html');
+
+});
 				//dependency -- it will be executed before
 gulp.task('copy',['clean'], function(){
 			//all directories and files
@@ -17,12 +25,33 @@ gulp.task('clean', function(){
 });
 
 // npm run gulp build-img
-gulp.task('build-img',['copy'], function(){
+gulp.task('build-img', function(){
 	 
 	gulp.src('dist/img/**/*') //<-- Process files inside folder **
 		.pipe(imagemin()) //<--module 
 		.pipe( gulp.dest('dist/img') );  
 
 }); 
+
+//concat
+gulp.task('build-js', function(){
+
+	gulp.src(['dist/js/main.js', 'dist/js/controllers/fotos-controller'])
+		.pipe(concat('js/all.js'))
+		.pipe(gulp.dest('dist/js'));
+
+});
+
+//replace files
+gulp.task('build-html', function(){
+
+	gulp.src('dist/**/*.html')
+		.pipe(htmlReplace({
+			js: 'all.js'
+		}))
+		.pipe(gulp.dest('dist'))
+
+
+});
 
 
